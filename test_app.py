9f740cdd1446b27fcfb40c1dcca225b30d4a15e3
karenRojas07 <karen.rojas07@uptc.edu.co>
2025-05-05 21:19:24 -0500

    Initial commit: Hola Mundo Flask app with tests

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.data == b'¡Hola Mundo!'
