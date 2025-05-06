0cbd6bb72ff8bbad17d0d5748743c6e3c16748b0
karenRojas07 <karen.rojas07@uptc.edu.co>
2025-05-06 10:32:23 -0500

    Cambio visual para prueba de despliegue continuo

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from prometheus_client import make_wsgi_app, Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
 app = Flask(__name__)
+
+# Configurar métricas
+REQUEST_COUNT = Counter('app_requests_total', 'Total web app requests')
 
 @app.route('/')
 def hello_world():
-    return '¡Hola Mundo!'
+    REQUEST_COUNT.inc()
+    return '¡Hola Mundo con python realizando un despliegue continuo!'
+
+# Añadir endpoint para métricas
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
